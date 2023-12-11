857013b38f1faf4dce2555ea63586f3286488315
yon-dev <152448145+yon-dev@users.noreply.github.com>
2023-12-11 12:25:26 -0500

    Create basic menu

--- app.js
+++ app.js
@@ -4,8 +4,28 @@ let sortFilter = { prop: 'yachtski', desc: true };
 let scoreFilter = { lower: 0, upper: 100 };
 let textFilter = '';
 
+let menuData = [
+  { url: 'https://sites.libsyn.com/483279', title: 'The Yacht Or Nyacht Podcast' },
+  { url: 'https://sites.libsyn.com/483282', title: 'Billion Dollar Record Club' },
+  { url: 'https://open.spotify.com/playlist/6kVRZHsbV6LZEBiqzSzFvq', title: 'Certified Yacht Rock Playlist' },
+  { url: 'https://docs.google.com/spreadsheets/d/1IZWQGzd3jQYOONuIi8eKBonA4lZ0xG1J4azf3dn81zk/edit', title: 'The Yachtski Scale' },
+]
+
 document.addEventListener('DOMContentLoaded', function () {
   createListFromData(visibleData);
+  populateMenu();
+
+  const menuBtn = document.getElementById('menuBtn');
+
+  menuBtn.addEventListener('click', (e) => {
+    const menuList = document.getElementById('menuList');
+
+    if (menuList.style.display === "none") {
+      menuList.style.display = "block";
+    } else {
+      menuList.style.display = "none";
+    }
+  })
 
   const scoreFilters = document.getElementsByClassName('score-filter');
   for (let filter of scoreFilters) {
@@ -56,6 +76,25 @@ function createListFromData() {
         songList.appendChild(row);
       })
     })
+}
+
+function populateMenu() {
+  // Generate menu list items
+  const menuList = document.getElementById('menuList');
+  menuList.innerHTML = '';
+
+  menuData.forEach((item) => {
+    const listItem = document.createElement('div');
+    listItem.className = 'menu-list-item';
+    listItem.innerHTML = item.title;
+
+    listItem.addEventListener('click', (e) => {
+      menuList.style.display = 'none';
+      window.open(item.url, '_blank');
+    });
+
+    menuList.appendChild(listItem);
+  })
 }
 
 function onSort(prop) {
