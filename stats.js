f433972ea7406747958b94d3aa5ab8782c59a01f
yon-dev <152448145+yon-dev@users.noreply.github.com>
2024-11-24 10:46:13 -0500

    Expand stats

--- stats.js
+++ stats.js
@@ -1,22 +1,25 @@
-let songs = Array.from(data);
-let hosts = ['jd', 'hunter', 'steve', 'dave'];
-let currentHost = 'jd';
+const songs = Array.from(data);
+const hosts = ['jd', 'hunter', 'steve', 'dave'];
+const currentHost = 'dave';
+const otherHosts = hosts.filter(hostName => hostName != currentHost);
 
 // Initialize stats object
 var hostStats =  {
   totals: { essential: 0, yacht: 0, nyacht: 0 },
-  disagreements: {},
-  dissent: { amount: 0, song: {} }
+  dissents: { yacht: [], nyacht: [] },
+  disagreements: { yacht: {}, nyacht: {} },
+  weirdEssentials: []
 };
 
-function statsFor(host) {
-  let otherHosts = hosts.filter(hostName => hostName != host);
-
+function generateStats() {
   // Construct disagreements
-  otherHosts.forEach(hostName => hostStats.disagreements[hostName] = { disagreement: 0, song: {}});
+  otherHosts.forEach(hostName => {
+    hostStats.disagreements.yacht[hostName] = { song: {}, difference: 0, otherScore: 0 };
+    hostStats.disagreements.nyacht[hostName] = { song: {}, difference: 0, otherScore: 0 };
+  });
 
   songs.forEach(song => {
-    let hostScore = song[host + '_score'];
+    let hostScore = song[currentHost + '_score'];
     
     // Update totals
     if (hostScore >= 90) {
@@ -28,52 +31,63 @@ function statsFor(host) {
     else {
       hostStats.totals.nyacht += 1;
     }
-    
-    // Update disagreements and dissent
-    otherHostsScoreTotal = 0;
-    otherHosts.forEach(hostName => {
-      otherHostScore = song[hostName + '_score'];
-      otherHostsScoreTotal += otherHostScore;
-      songDisagreement = Math.abs(hostScore - otherHostScore);
+  
+    var otherHostsScoreTotal = 0;
 
-      if (songDisagreement > hostStats.disagreements[hostName].disagreement) {
-        hostStats.disagreements[hostName] = { disagreement: songDisagreement, song: song }
+    otherHosts.forEach(hostName => {
+      var otherHostScore = song[hostName + '_score'];
+      otherHostsScoreTotal += otherHostScore;
+      var songDisagreement = hostScore - otherHostScore;
+
+      if (songDisagreement > 0 && songDisagreement > hostStats.disagreements.yacht[hostName].difference) {
+        hostStats.disagreements.yacht[hostName] = { difference: songDisagreement, otherScore: otherHostScore, song: song };
+      }
+      else if (songDisagreement < 0 && songDisagreement < hostStats.disagreements.nyacht[hostName].difference) {
+        hostStats.disagreements.nyacht[hostName] = { difference: songDisagreement, otherScore: otherHostScore, song: song };
       }
     });
 
-    dissentAmount = Math.abs(hostScore - (otherHostsScoreTotal / otherHosts.length));
+    // Update dissents
+    var othersAvg = (otherHostsScoreTotal / otherHosts.length);
+    var dissentAmount = hostScore - othersAvg;
 
-    if (dissentAmount > hostStats.dissent.amount) {
-      hostStats.dissent.amount = dissentAmount;
-      hostStats.dissent.song = song;
+    if (dissentAmount > 0) {
+      if (hostStats.dissents.yacht.length < 3) {
+        hostStats.dissents.yacht.push({ difference: dissentAmount, othersAvg: othersAvg, song: song });
+      } else if (dissentAmount > hostStats.dissents.yacht[2].difference) {
+        hostStats.dissents.yacht[2] = { difference: dissentAmount, othersAvg: othersAvg, song: song };
+      }
+    }
+    else {
+      if (hostStats.dissents.nyacht.length < 3) {
+        hostStats.dissents.nyacht.push({ difference: dissentAmount, othersAvg: othersAvg, song: song });
+      } else if (dissentAmount < hostStats.dissents.nyacht[2].difference) {
+        hostStats.dissents.nyacht[2] = { difference: dissentAmount, othersAvg: othersAvg, song: song };
+      }
+    }
+    
+    hostStats.dissents.yacht.sort((a, b) => b.difference - a.difference);
+    hostStats.dissents.nyacht.sort((a, b) => a.difference - b.difference);
+
+    // Update weird essentials
+    var otherHostAvg = (otherHostsScoreTotal / otherHosts.length);
+    if ((hostScore >= 90) && (otherHostAvg < 80)) {
+      hostStats.weirdEssentials.push({ otherScore: otherHostAvg, song: song });
     }
   });
 };
 
 // On page load, generate HTML elements
 document.addEventListener('DOMContentLoaded', function () {
-  statsFor(currentHost);
+  generateStats();
 
   const hostNameEls = Array.from(document.getElementsByClassName('host-name'));
   hostNameEls.forEach(el => el.textContent = currentHost); // Now you can use array methods
 
   generateTotalsElement();
-
-  // Create element for dissent
-  const dissentEl = document.getElementById('dissent');
-
-  var dissentStatBar = generateStatBar(hostStats.dissent.song[currentHost + '_score'], hostStats.dissent.amount, hostStats.dissent.song);
-  dissentEl.appendChild(dissentStatBar);
-
-  // Create element for disagreements
-  const disagreementsEl = document.getElementById('disagreements');
-
-  for (const key in hostStats.disagreements) {
-    disagreement = hostStats.disagreements[key];
-    
-    var disagreementStatBar = generateStatBar(disagreement.song[currentHost + '_score'], disagreement.disagreement, disagreement.song);
-    disagreementsEl.appendChild(disagreementStatBar);
-  }
+  generateDissentElement();
+  generateDisagreementElement();
+  generateWeirdEssentialsElement();
 
   // Create element for totals
   function generateTotalsElement() {
@@ -93,6 +107,62 @@ document.addEventListener('DOMContentLoaded', function () {
     nyachtTotal.style.color = getColorForScore(0);
     nyachtTotal.textContent = 'Nyacht: ' + hostStats.totals.nyacht;
     totalsEl.appendChild(nyachtTotal);
+  };
+
+  function generateDissentElement() {
+    const yDissentEl = document.getElementById('yacht-dissent');
+    const nDissentEl = document.getElementById('nyacht-dissent');
+    
+    var dissentStatBarHeader = generateStatBarHeader('Others');
+    yDissentEl.appendChild(dissentStatBarHeader);
+    nDissentEl.appendChild(dissentStatBarHeader);
+    
+    hostStats.dissents.yacht.forEach((dissent) => {
+      var dissentStatBar = generateStatBar(dissent.song[currentHost + '_score'], dissent.othersAvg, dissent.song);
+      yDissentEl.appendChild(dissentStatBar);
+    });
+
+    hostStats.dissents.nyacht.forEach((dissent) => {
+      var dissentStatBar = generateStatBar(dissent.song[currentHost + '_score'], dissent.othersAvg, dissent.song);
+      nDissentEl.appendChild(dissentStatBar);
+    });
+  };
+
+  function generateDisagreementElement() {
+    const yDisagreementsEl = document.getElementById('yacht-disagreements');
+    const nDisagreementsEl = document.getElementById('nyacht-disagreements');
+
+    for (const key in hostStats.disagreements.yacht) {
+      disagreement = hostStats.disagreements.yacht[key];
+      
+      var disagreementStatBarHeader = generateStatBarHeader(key);
+      var disagreementStatBar = generateStatBar(disagreement.song[currentHost + '_score'], disagreement.otherScore, disagreement.song);
+      
+      yDisagreementsEl.appendChild(disagreementStatBarHeader);
+      yDisagreementsEl.appendChild(disagreementStatBar);
+    }
+
+    for (const key in hostStats.disagreements.nyacht) {
+      disagreement = hostStats.disagreements.nyacht[key];
+      
+      var disagreementStatBarHeader = generateStatBarHeader(key);
+      var disagreementStatBar = generateStatBar(disagreement.song[currentHost + '_score'], disagreement.otherScore, disagreement.song);
+      
+      nDisagreementsEl.appendChild(disagreementStatBarHeader);
+      nDisagreementsEl.appendChild(disagreementStatBar);
+    }
+  };
+
+  function generateWeirdEssentialsElement() {
+    const weirdEl = document.getElementById('weird-essentials');
+
+    var weirdStatBarHeader = generateStatBarHeader('Others');
+    weirdEl.appendChild(weirdStatBarHeader);
+    
+    hostStats.weirdEssentials.forEach((weirdEssential) => {
+      var weirdStatBar = generateStatBar(weirdEssential.song[currentHost + '_score'], weirdEssential.otherScore, weirdEssential.song);
+      weirdEl.appendChild(weirdStatBar);
+    });
   };
 
   // Creates stat bar element
@@ -120,5 +190,19 @@ document.addEventListener('DOMContentLoaded', function () {
     statBar.appendChild(otherScoreEl);
 
     return statBar;
+  };
+
+  function generateStatBarHeader (otherName) {
+    var statBarHeaderEl = document.createElement('div');
+    statBarHeaderEl.className = 'stat-bar-header';
+
+    [currentHost, '\u00A0', otherName].forEach((columnName) => {
+      var columnNameEl = document.createElement('div');
+      columnNameEl.innerText = columnName;
+
+      statBarHeaderEl.appendChild(columnNameEl);
+    });
+
+    return statBarHeaderEl;
   };
 });
